b3062d349a50014f75a88f3b745e460b272cf58e
Marian Kuzyk <mariankuzyk@gmail.com>
2022-06-27 01:12:35 +0300

    hook-modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,35 +1,38 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import s from './Modal.module.css';
 
-class Modal extends Component {
-    componentDidMount() {
-        window.addEventListener('keydown', this.modalClose); // слухач на клавіатуру
-    };
-    
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this.modalClose); // вимикаю слухач щоб не висів
-    };
-    
-    modalClose = event => {
-        // якщо натиснути еск чи клачнути на фон, то пропсами передасться функція закриття модалки
-        if (event.code === 'Escape' || event.target === event.currentTarget) {
-            this.props.onClick();
+function Modal({onClick, url, alt}) {
+
+    useEffect(() => {
+        const modalCloseEsc = event => {
+            // якщо натиснути еск чи клачнути на фон, то пропсами передасться функція закриття модалки
+            if (event.code === 'Escape') {
+                onClick();
+            };
         };
+
+        window.addEventListener('keydown', modalCloseEsc); // слухач на клавіатуру
+
+        return () => {
+            window.removeEventListener('keydown', modalCloseEsc); // вимикаю слухач щоб не висів
+        }
+    }, [onClick]);
+    
+    const modalClose = e => {
+        // якщо натиснути  на фон, то пропсами передасться функція закриття модалки
+        if (e.target === e.currentTarget) {
+            onClick();
+        }
     };
 
-    render() {
-        // console.log(this.props);
-        const { url, alt } = this.props
-
-        return (
-            <div className={s.overlay} onClick={this.modalClose}>
-                <div className={s.modal}>
-                    <img src={url} alt={alt} />
-                </div>
+    return (
+        <div className={s.overlay} onClick={modalClose}>
+            <div className={s.modal}>
+                <img src={url} alt={alt} />
             </div>
-        );
-    };
+        </div>
+    );
 };
 
 export default Modal;
