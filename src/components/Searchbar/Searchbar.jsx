b40548e80d3ce8d814df3f6e7fca965582a6f8fa
Marian Kuzyk <mariankuzyk@gmail.com>
2022-06-26 23:31:14 +0300

    hook-searchbar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,61 +1,55 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import s from './Searchbar.module.css'
 
-export default class Searchbar extends Component {
-    state = {
-        imageName: '',
-    }
+export default function Searchbar({onSubmit}) {
+    const [imageName, setImageName] = useState('');
 
-    handleNameChange = (e) => {
-        this.setState({
-            // стейт приймає значення з інпут
-            imageName: e.currentTarget.value.toLowerCase()
-        });
+    const handleNameChange = (e) => {
+        // стейт приймає значення з інпут
+        setImageName(e.currentTarget.value.toLowerCase())
     };
 
-    handleSubmit = e => {
+    const handleSubmit = e => {
         e.preventDefault();
 
         // якщо пошук пустий, то помилку викидаю
-        if (this.state.imageName.trim() === '') {
+        if (imageName.trim() === '') {
             alert('Ще раз таке зробиш, і я викличу поліцію');
             return
         };
 
         // onSubmit це назва пропсів, а не слухач подій
-        this.props.onSubmit(this.state.imageName);
+        onSubmit(imageName);
         // стераю стейт до пустого місця
-        this.setState({ imageName: ''});
+        setImageName('');
     };
 
-    render() {
-        return (
-            <header className={s.searchbar}>
-                <form onSubmit={ this.handleSubmit} className={s.searchForm}>
-                        <button
-                            type="submit"
-                            className={s.button}
-                        >
-                            <span className={s.buttonLabel}>Search</span>
-                        </button>
+    return (
+        <header className={s.searchbar}>
+            <form onSubmit={ handleSubmit} className={s.searchForm}>
+                    <button
+                        type="submit"
+                        className={s.button}
+                    >
+                        <span className={s.buttonLabel}>Search</span>
+                    </button>
 
-                    <label>
-                        <input
-                            className={s.input}
-                            name="imageName"
-                            type="text"
-                            value={this.state.imageName}
-                            onChange={this.handleNameChange}
-                            autoComplete="off"
-                            autoFocus
-                            placeholder="Search images and photos"
-                        />
-                    </label>
-                </form>
-            </header>
-        );
-    };
+                <label>
+                    <input
+                        className={s.input}
+                        name="imageName"
+                        type="text"
+                        value={imageName}
+                        onChange={handleNameChange}
+                        autoComplete="off"
+                        autoFocus
+                        placeholder="Search images and photos"
+                    />
+                </label>
+            </form>
+        </header>
+    );
 };
 
 Searchbar.propTypes = {
