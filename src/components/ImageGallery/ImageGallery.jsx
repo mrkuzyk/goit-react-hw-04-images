12f159a5c4c29f3e1f4251e137528eca33c8e8d2
Marian Kuzyk <mariankuzyk@gmail.com>
2022-06-28 01:50:04 +0300

    done

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,9 +1,9 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect} from "react";
 import PropTypes from 'prop-types';
 import s from './ImageGallery.module.css';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 import Button from '../Button/Button';
-import { StartSearch, ImageNotFound } from '../messageTitle/messageTitle';
+import { StartSearch } from '../messageTitle/messageTitle';
 import Loader from '../Loader/Loader';
 import Modal from '../Modal/Modal';
 
@@ -13,68 +13,56 @@ export default function ImageGallery({imageName}) {
     const [loader, setLoader] = useState(false);
     const [totalHits, setTotalHits] = useState(null);
     const [page, setPage] = useState(1);
+    const [perPage, setPerPage] = useState(12);
     const [searchName, setSearchName] = useState('');
     const [showModal, setShowModal] = useState(false);
     const [largeImage, setLargeImage] = useState('');
     const [imageTags, setImageTags] = useState('');
-
-    const perPage = 12;
 
     useEffect(() => {
         if (!imageName) {
             return
         }
 
-        // console.log('pre', page);
-        const startSearshPage = 1; // примусово починаю з першої сторінки
+        // якщо попереднє ім'я змінене то ставлю першу сторінку і очищую масив картинок
+        if (imageName !== searchName) {
+            setPage(1);
+            setImages([]);
+        }
+
+        // console.log('pre', images);
+        // const startSearshPage = 1; // примусово починаю з першої сторінки
         setLoader(true); // включаю лоадер
-        setImages([]);
-        fetch(`https://pixabay.com/api/?q=${imageName}&page=${startSearshPage}&key=27124011-562ac77f1fd2864e5ddfeb16c&image_type=photo&orientation=horizontal&per_page=${perPage}`)
+        fetch(`https://pixabay.com/api/?q=${imageName}&page=${page}&key=27124011-562ac77f1fd2864e5ddfeb16c&image_type=photo&orientation=horizontal&per_page=${perPage}`)
                 .then(response => {
                     if (response.ok) {
                         return response.json()
                     }
 
                     return Promise.reject (
-                        new Error(`Незнайдено зображень з ім'ям ${imageName}`)
+                        new Error(`Незнайдено зображень з ім'ям ${imageName}. Перезавантажте сторінку і спробуйте ще раз!`)
                     )
                 })
-                .then(images => {
-                    // console.log(images);
-                    setPage(1); // кожен новий пошук починається з 1 сторінки
+            .then(images => {
+                // console.log('рендер');
+                if (page > 1) {
+                    //якщо це не перша сторнка, значить натиснута кнопка завантажити більше
+                    setImages(prevImages => [...prevImages, ...images.hits]); // додаю наступні сторінки до попердніх
+                    setLoader(false);
+                } else {
+                    setPage(1);
                     setSearchName(imageName);
                     setImages(images.hits);
                     setLoader(false); // виключаю лоадер після загрузки
                     setTotalHits(images.totalHits);  // загальна кількість картинок
-                    // console.log('post', page);
-                })
+                    setPerPage(images.hits.length)
+                }
+            })
             .catch(error =>{
                 setError(error);
                 setLoader(false);
             })
-    },[imageName]);
-    
-
-    useEffect(() => {
-        if (!searchName) {
-            return
-        }
-        setLoader(true); //включаю лоадер
-
-        console.log('pre-page', page);
-        fetch(`https://pixabay.com/api/?q=${searchName}&page=${page + 1}&key=27124011-562ac77f1fd2864e5ddfeb16c&image_type=photo&orientation=horizontal&per_page=${perPage}`)
-            .then(response => {
-                return response.json()
-            })
-            .then(images => {
-                setImages(prevImages => [...prevImages, ...images.hits]); // додаю наступні сторінки до попердніх
-                setLoader(false);
-            })
-            .catch(error => {
-                setError(error);
-                setLoader(false);
-            })
-    }, [page, searchName]);
+    },[imageName, page, perPage]);
 
     const morePageClick = () => {
         setPage(prevPage => prevPage + 1); // записую нову сторінку в стейдж
@@ -91,20 +79,18 @@ export default function ImageGallery({imageName}) {
     const toggleModal = () => {
         setShowModal(prevShowModal => !prevShowModal);
     }
-        // const { imageName } = this.props;
-    // const pages = totalHits / perPage; //рахую кількість сторінок
 
         return (
             <div className="s.container">
                 {!imageName && <StartSearch/>}
-                {totalHits === 0 && 
+                {/* {totalHits === 0 && 
                     <ImageNotFound
                         name={ imageName}
                     />
-                }
-                {error && <h1>{error.message}</h1>}
+                } */}
+                {error && <h1 className={s.error}>{error.message}</h1>}
                 {loader && <Loader/> }
-                {images && 
+                {images && !error && 
                     <ul className={s.gallery}>
                         {images.map(( image ) =>
                             <ImageGalleryItem
@@ -114,7 +100,7 @@ export default function ImageGallery({imageName}) {
                             />)}
                     </ul>
                 }
-                {page < totalHits / perPage &&
+                {page < totalHits / perPage && !loader &&
                     <Button
                         onClick={morePageClick}
                     />
